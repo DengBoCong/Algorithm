372ee81120c20c2ef23c39db1495716743ca99e6
DBC <1210212670@qq.com>
2019-12-04 17:17:47 +0800

    题目4草稿

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 import com.dbc.leecode.Algorithm.Solution1_10.Solution3;
+import com.dbc.leecode.Algorithm.Solution1_10.Solution4;
 
 public class Main {
     public static void main(String[] args) {
-        
+        int[] nums1 = new int[]{3};
+        int[] nums2 = new int[]{-2,-1};
+        System.out.println(Solution4.Test4S2(nums1, nums2));
     }
 }
---
+++ src/com/dbc/leecode/Algorithm/Solution1_10/Solution4.java
@@ -0,0 +1,65 @@
+package com.dbc.leecode.Algorithm.Solution1_10;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @program: LeeCode
+ * @description:
+ * @author: DBC
+ * @create: 2019-12-04 12:18
+ **/
+public class Solution4 {
+    public static double Test4S1(int[] nums1, int[] nums2) {
+        //该方法出错
+
+        double sum1 = 0.00; //计算数组一的中位数
+        double sum2 = 0.00; //计算数组二的中位数
+
+        if(nums1.length != 0){
+            if(nums1.length % 2 == 0){
+                int nums1_left = nums1.length / 2 - 1;
+                int nums1_right = nums1_left + 1;
+
+                sum1 = (nums1[nums1_left] + nums1[nums1_right]) / 2.00;
+            }else sum1 = nums1[nums1.length / 2];
+        }
+
+        if(nums2.length != 0){
+            if(nums2.length % 2 == 0){
+                int nums2_left = nums2.length / 2 - 1;
+                int nums2_right = nums2_left + 1;
+
+                sum2 = (nums2[nums2_left] + nums2[nums2_right]) / 2.00;
+            }else sum2 = nums2[nums2.length / 2];
+        }
+
+        if(nums1.length == 0) return sum2;
+        else if(nums2.length == 0) return sum1;
+        else return (sum1 + sum2) / 2.00;
+
+    }
+
+    public static double Test4S2(int[] nums1, int[] nums2) {
+        List<Integer> array = new ArrayList<>(10);
+        //int maxSize = nums1.length > nums2.length ? nums1.length : nums2.length;
+
+        int size1 = nums1.length;
+        int size2 = nums2.length;
+        int index1 = 0;
+        int index2 = 0;
+
+        while(index1 != size1 || index2 != size2){
+            array.add(nums1[index1] < nums2[index2] ? nums1[index1] : nums2[index2]);
+            System.out.println(index1);
+            System.out.println(index2);
+            if(nums1[index1] < nums2[index2] && index1 < size1) index1++;
+            if(nums1[index1] > nums2[index2] && index2 < size2) index2++;
+        }
+
+        int sizeArray = array.size() / 2;
+        if(array.size() % 2 == 0) return (array.get(sizeArray - 1) + array.get(sizeArray)) / 2.00;
+        else return array.get(sizeArray);
+    }
+
+}
