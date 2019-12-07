d7c4e917a6dfd7a2b5d4039512f33fed22656580
DBC <1210212670@qq.com>
2019-12-07 11:17:39 +0800

    完成最大回文子串第一种解决方案

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import com.dbc.leecode.Algorithm.Solution1_10.LongestPalindrome;
 
 public class Main {
     public static void main(String[] args) {
-        String s = "baaa";
+        String s = "a";
         System.out.println(LongestPalindrome.longestPalindromeS1(s));
     }
 }
--- src/com/dbc/leecode/Algorithm/Solution1_10/LongestPalindrome.java
+++ src/com/dbc/leecode/Algorithm/Solution1_10/LongestPalindrome.java
@@ -3,8 +3,11 @@ package com.dbc.leecode.Algorithm.Solution1_10;
 public class LongestPalindrome {
     public static String longestPalindromeS1(String s) {
         int max = 1; //记录最大回文字符串的长度
+        int max1 = 1; //记录偶数最大回文字符串的长度
         int left = 0;
         int right = 0;
+        int left1 = 0;
+        int right1 = 0;
         int length = s.length();
 
         for(int i = 0; i < length; i++){
@@ -23,30 +26,28 @@ public class LongestPalindrome {
                 left = leftIndex + 1;
                 right = rightIndex;
             }
-        }
 
-        int q = 1;
-        int e = 1;
-        int z = 0;
-        int x = 0;
-        for (int i = 0; i < length; i++){
-            z = i;
-            for (int j = i + 1; j < length; j++){
-                if(s.charAt(i) == s.charAt(j)) q++;
-                else {
-                    if(e < q){
-                        e = q;
-                        z = i;
-                        x = j + 1;
-                    }
-                    i = j;
-                    break;
-                }
+            int len1 = 0;
+            int leftIndex1 = i;
+            int rightIndex1 = i + 1;
+            while(leftIndex1 >= 0 && rightIndex1 < length){
+                if(s.charAt(leftIndex1) == s.charAt(rightIndex1)){
+                    len1 += 2;
+                    leftIndex1--;
+                    rightIndex1++;
+                }else break;
+            }
+            if(max1 < len1){
+                max1 = len1;
+                left1 = leftIndex1 + 1;
+                right1 = rightIndex1;
             }
         }
 
-        if(q < max) return s.substring(left, right);
-        else return s.substring(z, x);
+        if(max1 < max) return s.substring(left, right);
+        else if(s.equals("")) return "";
+        else if(max1 == 1 && max == 1) return s.substring(0,1);
+        else return s.substring(left1, right1);
     }
 
     /*public static String longOalindromeS1(int leftIndex, int rightIndex, String s){
