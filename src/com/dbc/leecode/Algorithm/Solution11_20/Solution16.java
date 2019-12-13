7a62166bc9ae10ddd593b434b44b19023216f326
DBC <1210212670@qq.com>
2019-12-13 23:40:15 +0800

    草稿

---
+++ src/com/dbc/leecode/Algorithm/Solution11_20/Solution16.java
@@ -0,0 +1,22 @@
+package com.dbc.leecode.Algorithm.Solution11_20;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @program: AlgorithmProject
+ * @description:
+ * @author: DBC
+ * @create: 2019-12-13 23:25
+ **/
+public class Solution16 {
+    public List<List<Integer>> threeSumS1(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++){
+            int left = 0;
+            int right = 0;
+        }
+        return  result;
+    }
+}
