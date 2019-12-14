6731317c247ead1ded30917a0bbd1892247b49ee
DBC <1210212670@qq.com>
2019-12-14 21:21:47 +0800

    三数之和

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import com.dbc.leecode.Algorithm.Solution1_10.*;
 
 public class Main {
     public static void main(String[] args) {
-        String s[] = new String[]{"flower","flow","flight"};
-        System.out.println(Solution15.longestCommonPrefixS2(s));
+        int s[] = new int[]{-1, 0, 1, 2, -1, -4};
+        System.out.println(Solution16.threeSumS1(s));
     }
 }
--- src/com/dbc/leecode/Algorithm/Solution11_20/Solution16.java
+++ src/com/dbc/leecode/Algorithm/Solution11_20/Solution16.java
@@ -1,6 +1,7 @@
 package com.dbc.leecode.Algorithm.Solution11_20;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -10,13 +11,29 @@ import java.util.List;
  * @create: 2019-12-13 23:25
  **/
 public class Solution16 {
-    public List<List<Integer>> threeSumS1(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++){
-            int left = 0;
-            int right = 0;
+    public static List<List<Integer>> threeSumS1(int[] nums) {
+        List<List<Integer>> ans = new ArrayList();
+        int len = nums.length;
+        if(nums == null || len < 3) return ans;
+        Arrays.sort(nums); // 排序
+        for (int i = 0; i < len ; i++) {
+            if(nums[i] > 0) break; // 如果当前数字大于0，则三数之和一定大于0，所以结束循环
+            if(i > 0 && nums[i] == nums[i-1]) continue; // 去重
+            int L = i+1;
+            int R = len-1;
+            while(L < R){
+                int sum = nums[i] + nums[L] + nums[R];
+                if(sum == 0){
+                    ans.add(Arrays.asList(nums[i],nums[L],nums[R]));
+                    while (L<R && nums[L] == nums[L+1]) L++; // 去重
+                    while (L<R && nums[R] == nums[R-1]) R--; // 去重
+                    L++;
+                    R--;
+                }
+                else if (sum < 0) L++;
+                else if (sum > 0) R--;
+            }
         }
-        return  result;
+        return ans;
     }
 }
