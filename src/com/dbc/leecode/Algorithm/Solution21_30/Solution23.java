b2b12b8852b7e39dca136c987027d6d4709299e3
DBC <1210212670@qq.com>
2019-12-17 23:42:32 +0800

    括号生成

--- src/com/dbc/leecode/Algorithm/Solution21_30/Solution23.java
+++ src/com/dbc/leecode/Algorithm/Solution21_30/Solution23.java
@@ -1,8 +1,47 @@
 package com.dbc.leecode.Algorithm.Solution21_30;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Solution23 {
     public List<String> generateParenthesis(int n) {
+        List<String> combinations = new ArrayList();
+        generateAll(new char[2 * n], 0, combinations);
+        return combinations;
+    }
+
+    public void generateAll(char[] current, int pos, List<String> result) {
+        if (pos == current.length) {
+            if (valid(current))
+                result.add(new String(current));
+        } else {
+            current[pos] = '(';
+            generateAll(current, pos+1, result);
+            current[pos] = ')';
+            generateAll(current, pos+1, result);
+        }
+    }
+
+    public boolean valid(char[] current) {
+        int balance = 0;
+        for (char c: current) {
+            if (c == '(') balance++;
+            else balance--;
+            if (balance < 0) return false;
+        }
+        return (balance == 0);
+    }
+
+    public List<String> generateParenthesisS3(int n) {
+        List<String> ans = new ArrayList();
+        if (n == 0) {
+            ans.add("");
+        } else {
+            for (int c = 0; c < n; ++c)
+                for (String left: generateParenthesisS3(c))
+                    for (String right: generateParenthesisS3(n-1-c))
+                        ans.add("(" + left + ")" + right);
+        }
+        return ans;
     }
 }
