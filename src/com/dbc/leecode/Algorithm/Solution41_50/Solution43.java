8c4dd02ca3eec62c9102292c4f47a2d2b4c54482
DengBoCong <1210212670@qq.com>
2020-01-03 18:53:53 +0800

    接雨水、字符串相乘

---
+++ src/com/dbc/leecode/Algorithm/Solution41_50/Solution43.java
@@ -0,0 +1,103 @@
+package com.dbc.leecode.Algorithm.Solution41_50;
+
+import java.util.Stack;
+
+public class Solution43 {
+    public int trapS1(int[] height) {
+        int sum = 0;
+        //最两端的列不用考虑，因为一定不会有水。所以下标从 1 到 length - 2
+        for (int i = 1; i < height.length - 1; i++) {
+            int max_left = 0;
+            //找出左边最高
+            for (int j = i - 1; j >= 0; j--) {
+                if (height[j] > max_left) {
+                    max_left = height[j];
+                }
+            }
+            int max_right = 0;
+            //找出右边最高
+            for (int j = i + 1; j < height.length; j++) {
+                if (height[j] > max_right) {
+                    max_right = height[j];
+                }
+            }
+            //找出两端较小的
+            int min = Math.min(max_left, max_right);
+            //只有较小的一段大于当前列的高度才会有水，其他情况不会有水
+            if (min > height[i]) {
+                sum = sum + (min - height[i]);
+            }
+        }
+        return sum;
+    }
+
+    public int trapS2(int[] height) {
+        int sum = 0;
+        int[] max_left = new int[height.length];
+        int[] max_right = new int[height.length];
+
+        for (int i = 1; i < height.length - 1; i++) {
+            max_left[i] = Math.max(max_left[i - 1], height[i - 1]);
+        }
+        for (int i = height.length - 2; i >= 0; i--) {
+            max_right[i] = Math.max(max_right[i + 1], height[i + 1]);
+        }
+        for (int i = 1; i < height.length - 1; i++) {
+            int min = Math.min(max_left[i], max_right[i]);
+            if (min > height[i]) {
+                sum = sum + (min - height[i]);
+            }
+        }
+        return sum;
+    }
+
+    public int trapS3(int[] height) {
+        int sum = 0;
+        int max_left = 0;
+        int max_right = 0;
+        int left = 1;
+        int right = height.length - 2; // 加右指针进去
+        for (int i = 1; i < height.length - 1; i++) {
+            //从左到右更
+            if (height[left - 1] < height[right + 1]) {
+                max_left = Math.max(max_left, height[left - 1]);
+                int min = max_left;
+                if (min > height[left]) {
+                    sum = sum + (min - height[left]);
+                }
+                left++;
+                //从右到左更
+            } else {
+                max_right = Math.max(max_right, height[right + 1]);
+                int min = max_right;
+                if (min > height[right]) {
+                    sum = sum + (min - height[right]);
+                }
+                right--;
+            }
+        }
+        return sum;
+    }
+
+    public int trapS4(int[] height) {
+        int sum = 0;
+        Stack<Integer> stack = new Stack<>();
+        int current = 0;
+        while (current < height.length) {
+            //如果栈不空并且当前指向的高度大于栈顶高度就一直循环
+            while (!stack.empty() && height[current] > height[stack.peek()]) {
+                int h = height[stack.peek()]; //取出要出栈的元素
+                stack.pop(); //出栈
+                if (stack.empty()) { // 栈空就出去
+                    break;
+                }
+                int distance = current - stack.peek() - 1; //两堵墙之前的距离。
+                int min = Math.min(height[stack.peek()], height[current]);
+                sum = sum + distance * (min - h);
+            }
+            stack.push(current); //当前指向的墙入栈
+            current++; //指针后移
+        }
+        return sum;
+    }
+}
---
+++ src/com/dbc/leecode/Algorithm/Solution41_50/Solution44.java
@@ -0,0 +1,26 @@
+package com.dbc.leecode.Algorithm.Solution41_50;
+
+public class Solution44 {
+    public String multiply(String num1, String num2) {
+        if (num1.equals("0") || num2.equals("0")) {
+            return "0";
+        }
+        int[] res = new int[num1.length() + num2.length()];
+        for (int i = num1.length() - 1; i >= 0; i--) {
+            int n1 = num1.charAt(i) - '0';
+            for (int j = num2.length() - 1; j >= 0; j--) {
+                int n2 = num2.charAt(j) - '0';
+                int sum = (res[i + j + 1] + n1 * n2);
+                res[i + j + 1] = sum % 10;
+                res[i + j] += sum / 10;
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < res.length; i++) {
+            if (i == 0 && res[i] == 0) continue;
+            result.append(res[i]);
+        }
+        return result.toString();
+    }
+}
