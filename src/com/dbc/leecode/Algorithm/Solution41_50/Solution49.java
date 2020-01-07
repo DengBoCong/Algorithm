32300ac08cfe9adb03fc1c9e5fd794f3daf4cc9f
DengBoCong <1210212670@qq.com>
2020-01-07 18:47:32 +0800

    全排列2、旋转图片

---
+++ src/com/dbc/leecode/Algorithm/Solution41_50/Solution49.java
@@ -0,0 +1,60 @@
+package com.dbc.leecode.Algorithm.Solution41_50;
+
+public class Solution49 {
+    public void rotateS1(int[][] matrix) {
+        int n = matrix.length;
+
+        // transpose matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int tmp = matrix[j][i];
+                matrix[j][i] = matrix[i][j];
+                matrix[i][j] = tmp;
+            }
+        }
+        // reverse each row
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - j - 1];
+                matrix[i][n - j - 1] = tmp;
+            }
+        }
+    }
+
+    public void rotateS2(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n / 2 + n % 2; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int[] tmp = new int[4];
+                int row = i;
+                int col = j;
+                for (int k = 0; k < 4; k++) {
+                    tmp[k] = matrix[row][col];
+                    int x = row;
+                    row = col;
+                    col = n - 1 - x;
+                }
+                for (int k = 0; k < 4; k++) {
+                    matrix[row][col] = tmp[(k + 3) % 4];
+                    int x = row;
+                    row = col;
+                    col = n - 1 - x;
+                }
+            }
+        }
+    }
+
+    public void rotateS3(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < (n + 1) / 2; i ++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[n - 1 - j][i];
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1];
+                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 -i];
+                matrix[j][n - 1 - i] = matrix[i][j];
+                matrix[i][j] = temp;
+            }
+        }
+    }
+}
