00d8bfb665130d378a8c6fd2f27f10879488bbc6
DengBoCong <1210212670@qq.com>
2020-01-08 21:28:41 +0800

    字母异位词分组、Pow(x,n)

---
+++ src/com/dbc/leecode/Algorithm/Solution51_60/Solution51.java
@@ -0,0 +1,41 @@
+package com.dbc.leecode.Algorithm.Solution51_60;
+
+public class Solution51 {
+    private double fastPow(double x, long n) {
+        if (n == 0) {
+            return 1.0;
+        }
+        double half = fastPow(x, n / 2);
+        if (n % 2 == 0) {
+            return half * half;
+        } else {
+            return half * half * x;
+        }
+    }
+    public double myPowS1(double x, int n) {
+        long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+
+        return fastPow(x, N);
+    }
+
+    public double myPow(double x, int n) {
+        long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        double ans = 1;
+        double current_product = x;
+        for (long i = N; i > 0; i /= 2) {
+            if ((i % 2) == 1) {
+                ans = ans * current_product;
+            }
+            current_product = current_product * current_product;
+        }
+        return ans;
+    }
+}
