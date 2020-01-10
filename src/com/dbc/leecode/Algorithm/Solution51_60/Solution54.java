3613621700bfc963267c266c79f79dfa671c1fe1
DengBoCong <1210212670@qq.com>
2020-01-10 17:31:18 +0800

    最大子序和、螺旋矩阵

---
+++ src/com/dbc/leecode/Algorithm/Solution51_60/Solution54.java
@@ -0,0 +1,59 @@
+package com.dbc.leecode.Algorithm.Solution51_60;
+
+public class Solution54 {
+    public int maxSubArrayS1(int[] nums) {
+        int n = nums.length;
+        int currSum = nums[0], maxSum = nums[0];
+
+        for(int i = 1; i < n; ++i) {
+            currSum = Math.max(nums[i], currSum + nums[i]);
+            maxSum = Math.max(maxSum, currSum);
+        }
+        return maxSum;
+    }
+
+    public int crossSum(int[] nums, int left, int right, int p) {
+        if (left == right) return nums[left];
+
+        int leftSubsum = Integer.MIN_VALUE;
+        int currSum = 0;
+        for(int i = p; i > left - 1; --i) {
+            currSum += nums[i];
+            leftSubsum = Math.max(leftSubsum, currSum);
+        }
+
+        int rightSubsum = Integer.MIN_VALUE;
+        currSum = 0;
+        for(int i = p + 1; i < right + 1; ++i) {
+            currSum += nums[i];
+            rightSubsum = Math.max(rightSubsum, currSum);
+        }
+
+        return leftSubsum + rightSubsum;
+    }
+
+    public int helper(int[] nums, int left, int right) {
+        if (left == right) return nums[left];
+
+        int p = (left + right) / 2;
+
+        int leftSum = helper(nums, left, p);
+        int rightSum = helper(nums, p + 1, right);
+        int crossSum = crossSum(nums, left, right, p);
+
+        return Math.max(Math.max(leftSum, rightSum), crossSum);
+    }
+
+    public int maxSubArrayS2(int[] nums) {
+        return helper(nums, 0, nums.length - 1);
+    }
+
+    public int maxSubArray(int[] nums) {
+        int n = nums.length, maxSum = nums[0];
+        for(int i = 1; i < n; ++i) {
+            if (nums[i - 1] > 0) nums[i] += nums[i - 1];
+            maxSum = Math.max(nums[i], maxSum);
+        }
+        return maxSum;
+    }
+}
---
+++ src/com/dbc/leecode/Algorithm/Solution51_60/Solution55.java
@@ -0,0 +1,52 @@
+package com.dbc.leecode.Algorithm.Solution51_60;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution55 {
+    public List<Integer> spiralOrderS1(int[][] matrix) {
+        List ans = new ArrayList();
+        if (matrix.length == 0) return ans;
+        int R = matrix.length, C = matrix[0].length;
+        boolean[][] seen = new boolean[R][C];
+        int[] dr = {0, 1, 0, -1};
+        int[] dc = {1, 0, -1, 0};
+        int r = 0, c = 0, di = 0;
+        for (int i = 0; i < R * C; i++) {
+            ans.add(matrix[r][c]);
+            seen[r][c] = true;
+            int cr = r + dr[di];
+            int cc = c + dc[di];
+            if (0 <= cr && cr < R && 0 <= cc && cc < C && !seen[cr][cc]){
+                r = cr;
+                c = cc;
+            } else {
+                di = (di + 1) % 4;
+                r += dr[di];
+                c += dc[di];
+            }
+        }
+        return ans;
+    }
+
+    public List < Integer > spiralOrderS2(int[][] matrix) {
+        List ans = new ArrayList();
+        if (matrix.length == 0)
+            return ans;
+        int r1 = 0, r2 = matrix.length - 1;
+        int c1 = 0, c2 = matrix[0].length - 1;
+        while (r1 <= r2 && c1 <= c2) {
+            for (int c = c1; c <= c2; c++) ans.add(matrix[r1][c]);
+            for (int r = r1 + 1; r <= r2; r++) ans.add(matrix[r][c2]);
+            if (r1 < r2 && c1 < c2) {
+                for (int c = c2 - 1; c > c1; c--) ans.add(matrix[r2][c]);
+                for (int r = r2; r > r1; r--) ans.add(matrix[r][c1]);
+            }
+            r1++;
+            r2--;
+            c1++;
+            c2--;
+        }
+        return ans;
+    }
+}
