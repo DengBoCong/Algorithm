a505e8d94a4227a3dc65afb0bab3a26449f9aff9
DBC <1210212670@qq.com>
2020-03-17 15:57:31 +0800

    '数据库相关'

---
+++ src/com/dbc/leecode/Algorithm/Solution171_180/Solution174.java
@@ -0,0 +1,8 @@
+package com.dbc.leecode.Algorithm.Solution171_180;
+
+public class Solution174 {
+//    select Email
+//    from Person
+//    group by Email
+//    having count(Email) > 1;
+}
---
+++ src/com/dbc/leecode/Algorithm/Solution171_180/Solution176.java
@@ -0,0 +1,21 @@
+package com.dbc.leecode.Algorithm.Solution171_180;
+
+public class Solution176 {
+//    SELECT
+//    Department.name AS 'Department',
+//    Employee.name AS 'Employee',
+//    Salary
+//            FROM
+//    Employee
+//            JOIN
+//    Department ON Employee.DepartmentId = Department.Id
+//    WHERE
+//            (Employee.DepartmentId , Salary) IN
+//    (   SELECT
+//    DepartmentId, MAX(Salary)
+//    FROM
+//            Employee
+//    GROUP BY DepartmentId
+//	)
+//    ;
+}
