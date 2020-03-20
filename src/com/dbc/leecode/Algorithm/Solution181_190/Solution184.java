b7e3163a8a5d0bcd1a9ae76dad88c90287a665f6
DBC <1210212670@qq.com>
2020-03-20 09:58:55 +0800

    '统计词频、有效电话号码、转置文件'

---
+++ src/com/dbc/leecode/Algorithm/Solution181_190/Solution184.java
@@ -0,0 +1,5 @@
+package com.dbc.leecode.Algorithm.Solution181_190;
+
+public class Solution184 {
+//    grep -P '^([0-9]{3}-|\([0-9]{3}\) )[0-9]{3}-[0-9]{4}$' file.txt
+}
---
+++ src/com/dbc/leecode/Algorithm/Solution181_190/Solution185.java
@@ -0,0 +1,18 @@
+package com.dbc.leecode.Algorithm.Solution181_190;
+
+public class Solution185 {
+//    awk '{
+//            for (i=1;i<=NF;i++){
+//        if (NR==1){
+//            res[i]=$i
+//        }
+//        else{
+//            res[i]=res[i]" "$i
+//        }
+//    }
+//}END{
+//        for(j=1;j<=NF;j++){
+//        print res[j]
+//        }
+//        }' file.txt
+}
